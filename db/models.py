8a18e48c0addfeea44cd7a614182d9598c7de101
Oleksii Mishchenko <alex3hard@gmail.com>
2025-12-03 17:07:10 +0200

    done project 1.1

--- db/models.py
+++ db/models.py
@@ -46,7 +46,6 @@ class Order(models.Model):
         return str(self.created_at)
 
 
-
 class Ticket(models.Model):
     movie_session = models.ForeignKey(MovieSession, on_delete=models.CASCADE)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
